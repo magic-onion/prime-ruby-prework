ff4da8ad675f20ad462592c4175030374725ac8a
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-25 23:18:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,5 @@
 # Add  code here!
 def prime?(number)
-  
-     def modulo_method(number)
        arr = [2 ,3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15]
        arr.each {|i| number % 2}
      end  
