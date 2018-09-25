3a1c9bada8d5bf4540e2a59a0b17110c8fab7e84
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-25 23:28:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,19 @@
 # Add  code here!
 def prime?(number)
+  tester = (2..Math.sqrt(number)).to_a 
+    for i in tester
+      if number % i == 0 
+        false
+      else
+        true
+    end
 
 end
   
 #takes in an Integer from an Array
-tests that number to see if it 
-#returns a boolean
+#provides a range of numbers from 2 to the square root of the integer
+#tests that Integer to see if it modulos with a number from the range
+
+
+#returns a boolean of false if the modulo equals 0 
+#returns true otherwise
