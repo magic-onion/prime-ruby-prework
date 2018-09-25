9dd5d9e656723f695de30038a687bce527e40425
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-25 23:30:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,14 +6,15 @@ def prime?(number)
         false
       else
         true
+      end
     end
 
 end
   
-#takes in an Integer from an Array
-#provides a range of numbers from 2 to the square root of the integer
-#tests that Integer to see if it modulos with a number from the range
+##takes in an Integer from an Array##
+##provides a range of numbers from 2 to the square root of the integer##
+#tests that Integer to see if it modulos with a number from the range##
 
 
 #returns a boolean of false if the modulo equals 0 
-#returns true otherwise
+#returns true otherwise##
