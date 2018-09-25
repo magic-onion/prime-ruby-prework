55dbb0435a9fe69d59eaf1e350e3b87a37f39315
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-25 23:18:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,5 @@ def prime?(number)
        arr = [2 ,3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15]
        arr.each {|i| number % 2}
      end  
-  if modulo_method == 2
-    false 
-  else 
-    true
-  end 
 end
   
