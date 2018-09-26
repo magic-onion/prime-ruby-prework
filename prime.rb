37685c2942ca33e8639c8170125d3c4c36aae138
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-26 00:09:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,14 +2,11 @@
 def prime?(number)
   if number <= 1
     false
-  elsif 
-  
-  tester = Array(2..number-1)
-  if number == 2 | number == 3
+  elsif number == 2 | number == 3
     true
-  elsif number <= 1 
-    false
-  elsif tester.any? {|i| number % i == 0}
+  end
+  tester = Array(2..number-1)
+  if tester.any? {|i| number % i == 0}
     false
   else
     true
