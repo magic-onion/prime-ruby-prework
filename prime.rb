04465806e16be94fd7d94753728f9871ceebc748
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-26 00:16:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number === 2 || number === 3
+    return true
+  end
+  tester = Array(2..number-1)
+  if tester.any? {|i| number % i == 0}
+    false
+  else
+    true
+  end
+end
